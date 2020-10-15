c0aca97aa6a19b8d45699de700e1f175476d4086
AndyPythonCode <andyarciniegas02@gmail.com>
2020-10-14 20:31:15 -0400

    Postgresql

--- Portafolio/settings/database.py
+++ Portafolio/settings/database.py
@@ -6,3 +6,20 @@ SQLITE3 = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+POSTGRESQL = {
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'd44u9sqp0vbluj',
+
+        'USER': 'qdfznnqdzjvqbe',
+
+        'PASSWORD': '1abf730b3f91c01f42b81f6a4990a400b649cc7e4d69281c718d69d85d7f43a3',
+
+        'HOST': 'ec2-54-152-40-168.compute-1.amazonaws.com',
+
+        'PORT': '5432',
+    }
+}
--- Portafolio/settings/production.py
+++ Portafolio/settings/production.py
@@ -1,6 +1,6 @@
 import django_heroku
 from .base import *
-from .database import SQLITE3
+from .database import POSTGRESQL
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -10,6 +10,6 @@ ALLOWED_HOSTS = ['andyarciniega.herokuapp.com', '127.0.0.1']
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = SQLITE3
+DATABASES = POSTGRESQL
 
 django_heroku.settings(locals())
