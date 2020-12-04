06d4ed3ffd16d606733c204c96fe2f7d3a9e687b
AndyPythonCode <andyarciniegas02@gmail.com>
2020-12-04 18:34:14 -0400

    hilo todo desde el inicio

--- app/unidad/urls.py
+++ app/unidad/urls.py
@@ -1,10 +1,8 @@
-from os import name
-from app.unidad.views import base
 from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.base, name='home'),
+    path('home', views.base, name='home'),
     path('biografia/', views.biografia, name='biografia'),
     path('etiquetas/', views.etiqueta, name='etiqueta'),
     path('banreservas/', views.banreservas, name='banco'),
